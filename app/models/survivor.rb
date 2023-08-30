81e9a325d57d86345c9dc6ef54732f2d90e29262
Leo Garcia <leogarcia61@gmail.com>
2023-08-30 11:43:09 +0100

    Add models and relationships

---
+++ app/models/survivor.rb
@@ -0,0 +1,3 @@
+class Survivor < ApplicationRecord
+  belongs_to :group
+end
---
+++ test/models/zombie_horde_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ZombieHordeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
